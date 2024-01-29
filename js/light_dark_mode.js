dc897e53d8a2c83f3a2ab86ddbbc756fea3b2ec9
Harshil Singh <hars.sin.hil@gmail.com>
2024-01-29 19:10:35 +0530

    Figured out session storage

--- js/light_dark_mode.js
+++ js/light_dark_mode.js
@@ -1,19 +1,39 @@
-var current_mode = 1;
+function check_mode()
+{
+    if(sessionStorage.getItem("current_mode")===null)
+    {
+        sessionStorage.setItem("current_mode", "dark");
+        const body_element = document.getElementsByTagName("body")[0];
+        body_element.classList.add("dark_mode");
+    }
+    else{
+        if(sessionStorage.getItem("current_mode")=="light")
+        {
+            const body_element = document.getElementsByTagName("body")[0];
+            body_element.classList.add("light_mode");
+        }
+        else
+        {
+            const body_element = document.getElementsByTagName("body")[0];
+            body_element.classList.add("dark_mode");
+        }
+    }
+}
 
 function switch_mode()
 {
-    if(current_mode==1)
+    if(sessionStorage.getItem("current_mode")=="light")
     {
         const body_element = document.getElementsByTagName("body")[0];
         body_element.classList.remove("light_mode");
         body_element.classList.add("dark_mode");
-        current_mode = 0;
+        sessionStorage.setItem("current_mode", "dark")
     }
     else
     {
         const body_element = document.getElementsByTagName("body")[0];
         body_element.classList.remove("dark_mode");
         body_element.classList.add("light_mode");
-        current_mode = 1;
+        sessionStorage.setItem("current_mode", "light");
     }
 }
