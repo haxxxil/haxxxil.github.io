797e81007597ade83beec2fc9cc7015f7185d11d
Harshil Singh <hars.sin.hil@gmail.com>
2024-02-02 15:24:06 +0530

    Finished top bar

--- js/light_dark_mode.js
+++ js/light_dark_mode.js
@@ -5,17 +5,20 @@ function check_mode()
         sessionStorage.setItem("current_mode", "dark");
         const body_element = document.getElementsByTagName("body")[0];
         body_element.classList.add("dark_mode");
+        document.getElementById("mode_switcher").src = "/images/light_mode_icon.png"
     }
     else{
         if(sessionStorage.getItem("current_mode")=="light")
         {
             const body_element = document.getElementsByTagName("body")[0];
             body_element.classList.add("light_mode");
+            document.getElementById("mode_switcher").src = "/images/dark_mode_icon.png"
         }
         else
         {
             const body_element = document.getElementsByTagName("body")[0];
             body_element.classList.add("dark_mode");
+            document.getElementById("mode_switcher").src = "/images/light_mode_icon.png"
         }
     }
 }
@@ -28,6 +31,7 @@ function switch_mode()
         body_element.classList.remove("light_mode");
         body_element.classList.add("dark_mode");
         sessionStorage.setItem("current_mode", "dark")
+        document.getElementById("mode_switcher").src = "/images/light_mode_icon.png"
     }
     else
     {
@@ -35,5 +39,6 @@ function switch_mode()
         body_element.classList.remove("dark_mode");
         body_element.classList.add("light_mode");
         sessionStorage.setItem("current_mode", "light");
+        document.getElementById("mode_switcher").src = "/images/dark_mode_icon.png"
     }
 }
