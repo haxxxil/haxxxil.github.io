f858c61592de833498ca95623b6fffabfaaadaea
Harshil Singh <hars.sin.hil@gmail.com>
2024-02-04 21:42:34 +0530

    Almost done

--- js/light_dark_mode.js
+++ js/light_dark_mode.js
@@ -42,3 +42,93 @@ function switch_mode()
         document.getElementById("mode_switcher").src = "/images/dark_mode_icon.png"
     }
 }
+
+function check_like_dislike(blog_name)
+{
+    check_mode();
+    console.log("entered check like dislike\n");
+    if(localStorage.getItem(blog_name+"_like_count")===null)
+    {
+        localStorage.setItem(blog_name+"_like_count", "0");
+    }
+    if(localStorage.getItem(blog_name+"_dislike_count")===null)
+    {
+        localStorage.setItem(blog_name+"_dislike_count", "0");
+    }
+    document.getElementById(blog_name+"_like_count").innerHTML = localStorage.getItem(blog_name+"_like_count");
+    document.getElementById(blog_name+"_dislike_count").innerHTML = localStorage.getItem(blog_name+"_dislike_count");
+    if(localStorage.getItem(blog_name+"_liked")===null)
+    {
+        localStorage.setItem(blog_name+"_liked", "0");
+    }
+    if(localStorage.getItem(blog_name+"_disliked")===null)
+    {
+        localStorage.setItem(blog_name+"_disliked", "0");
+    }
+    if(localStorage.getItem(blog_name+"_liked")=="1") 
+    {
+        document.getElementById(blog_name+"_like").classList.add("clicked");
+    }
+    if(localStorage.getItem(blog_name+"_disliked")=="1") 
+    {
+        document.getElementById(blog_name+"_dislike").classList.add("clicked");
+    }
+}
+
+function like(blog_name)
+{
+    if(localStorage.getItem(blog_name+"_liked")=="1")
+    {
+        localStorage.setItem(blog_name+"_liked", "0");
+        let y = parseInt(localStorage.getItem(blog_name+"_like_count"));
+        y = y - 1;
+        localStorage.setItem(blog_name+"_like_count", y.toString());
+        document.getElementById(blog_name+"_like").classList.remove("clicked");
+        document.getElementById(blog_name+"_like_count").innerHTML = localStorage.getItem(blog_name+"_like_count");
+        return;
+    }
+    if(localStorage.getItem(blog_name+"_disliked")=="1")
+    {
+        let x = parseInt(localStorage.getItem(blog_name+"_dislike_count"));
+        x = x-1;
+        localStorage.setItem(blog_name+"_dislike_count", x.toString());
+        localStorage.setItem(blog_name+"_disliked", "0");
+        document.getElementById(blog_name+"_dislike").classList.remove("clicked");
+        document.getElementById(blog_name+"_dislike_count").innerHTML = localStorage.getItem(blog_name+"_dislike_count");
+    }
+    localStorage.setItem(blog_name+"_liked", "1");
+    let y = parseInt(localStorage.getItem(blog_name+"_like_count"));
+    y = y + 1;
+    localStorage.setItem(blog_name+"_like_count", y.toString());
+    document.getElementById(blog_name+"_like").classList.add("clicked");
+    document.getElementById(blog_name+"_like_count").innerHTML = localStorage.getItem(blog_name+"_like_count");
+}
+
+function dislike(blog_name)
+{
+    if(localStorage.getItem(blog_name+"_disliked")=="1")
+    {
+        localStorage.setItem(blog_name+"_disliked", "0");
+        let y = parseInt(localStorage.getItem(blog_name+"_dislike_count"));
+        y = y - 1;
+        localStorage.setItem(blog_name+"_dislike_count", y.toString());
+        document.getElementById(blog_name+"_dislike").classList.remove("clicked");
+        document.getElementById(blog_name+"_dislike_count").innerHTML = localStorage.getItem(blog_name+"_dislike_count");
+        return;
+    }
+    if(localStorage.getItem(blog_name+"_liked")=="1")
+    {
+        let x = parseInt(localStorage.getItem(blog_name+"_like_count"));
+        x = x-1;
+        localStorage.setItem(blog_name+"_like_count", x.toString());
+        localStorage.setItem(blog_name+"_liked", "0");
+        document.getElementById(blog_name+"_like").classList.remove("clicked");
+        document.getElementById(blog_name+"_like_count").innerHTML = localStorage.getItem(blog_name+"_like_count");
+    }
+    localStorage.setItem(blog_name+"_disliked", "1");
+    let y = parseInt(localStorage.getItem(blog_name+"_dislike_count"));
+    y = y + 1;
+    localStorage.setItem(blog_name+"_dislike_count", y.toString());
+    document.getElementById(blog_name+"_dislike").classList.add("clicked");
+    document.getElementById(blog_name+"_dislike_count").innerHTML = localStorage.getItem(blog_name+"_dislike_count");
+}
